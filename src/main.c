231c8ce953597ed4cbae6aaad44b1cf5e4a64e93
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-03-11 17:04:43 +0100

    Basic primality testing

--- src/main.c
+++ src/main.c
@@ -1,5 +1,18 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+
+int isPrime(uint64_t p) {
+	for (uint64_t i = 2; i <= sqrt(p); ++i) {
+		if (p % i == 0)
+			return 0;
+	}
+	return 1;
+}
 
 int main() {
-	printf("Hello world");
+	printf("Testing for primality:\n");
+	for (uint64_t i = 1; i < 11; ++i) {
+		printf("%d is prime: %d\n", i, isPrime(i));
+	}
 }
