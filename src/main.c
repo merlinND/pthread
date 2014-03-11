e67b28e2ddd67b817271e4184e8999ddb593809c
Robin Ricard <ricard.robin@gmail.com>
2014-03-11 17:13:37 +0100

    Files

--- src/main.c
+++ src/main.c
@@ -1,5 +1,13 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdint.h>
 
 int main() {
 	printf("Hello world");
+
+  FILE * f = fopen("numbers.txt", 'r');
+
+  uint64_t number = 0;
+  while(fscanf(f, "%d\n", &number)) {
+    printPrimeFactors(number);
+  }
 }
