4245102375c58d348959b7e9e0cd50cc643c1807
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-03-11 23:18:03 +0100

    Take input from stdin + use two distinct mutex

--- src/main.c
+++ src/main.c
@@ -5,8 +5,13 @@
 
 #define MAX_FACTORS 64
 
+// ----- Structures
+// Memoization
+// TODO: a data structure that could hold the previous factorization results
+
 // ----- Module-wide variables
 static FILE * f;
+static pthread_mutex_t readMutex, outputMutex;
 
 int isPrime(uint64_t p) {
   for (uint64_t i = 2; i <= sqrt(p); ++i) {
@@ -18,6 +23,7 @@ int isPrime(uint64_t p) {
 
 // Compute the prime factorisation of <n> and returns the number of factors
 int getPrimeFactors(uint64_t n, uint64_t * destination) {
+  //printf("Starting to factorize %llu\n", n);
   uint64_t numberOfFactors = 0;
 
   uint64_t i = 2;
@@ -36,53 +42,49 @@ int getPrimeFactors(uint64_t n, uint64_t * destination) {
   return numberOfFactors;
 }
 
-void printPrimeFactors(uint64_t n, pthread_mutex_t * mutex) {
+void printPrimeFactors(uint64_t n) {
   uint64_t factors[MAX_FACTORS];
   int numberOfFactors = getPrimeFactors(n, factors);
 
-  // We lock the mutex to guarantee that output will appear in order
-  // TODO: use two different mutex for read and write
-  pthread_mutex_lock(mutex);
+  // We use a mutex to guarantee that output will appear in order
+  pthread_mutex_lock(&outputMutex);
   printf("%llu: ", n);
   for (int i = 0; i < numberOfFactors; ++i) {
     printf("%llu ", factors[i]);
   }
   printf("\n");
-  pthread_mutex_unlock(mutex);
+  pthread_mutex_unlock(&outputMutex);
 }
 
 void startJob(void * arg) {
-  pthread_mutex_t * mutex = arg;
-
   uint64_t number;
   int status = 0;
   while(status != -1) {
     // Get mutex in order to read into file
-    pthread_mutex_lock(mutex);
+    pthread_mutex_lock(&readMutex);
     status = fscanf(f, "%llu", &number);
     // Release the mutex as quickly as possible
-    pthread_mutex_unlock(mutex);
+    pthread_mutex_unlock(&readMutex);
 
     if (status != -1) {
-      printPrimeFactors(number, mutex);
+      printPrimeFactors(number);
     }
   }
 }
 
 int main() {
-  f = fopen("numbers.txt", "r");
+  f = stdin;
   if(f != NULL) {
     pthread_mutex_t mutex;
-    pthread_mutex_init(&mutex, NULL);
+    pthread_mutex_init(&readMutex, NULL);
+    pthread_mutex_init(&outputMutex, NULL);
 
     pthread_t thread1, thread2;
-    pthread_create(&thread1, NULL, startJob, (void *) &mutex);
-    pthread_create(&thread2, NULL, startJob, (void *) &mutex);
+    pthread_create(&thread1, NULL, startJob, NULL);
+    pthread_create(&thread2, NULL, startJob, NULL);
     pthread_join(thread2, NULL);
     pthread_join(thread1, NULL);
-
-    fclose(f);
   } else {
-    perror("Error opening numbers.txt");
+    perror("Error opening input file.");
   }
 }
