7a1354e94f6da7854e6951018b491ce3de2aa07d
Robin Ricard <ricard.robin@gmail.com>
2014-03-19 15:18:58 +0100

    CLI Options

--- src/main.c
+++ src/main.c
@@ -1,13 +1,39 @@
 #include <stdio.h>
 
 #include "sequential.h"
+#include "worker.h"
 
-int main() {
+
+void sequencialRun(FILE * f) {
+  initSequentialContext(f);
+  runOneJob();
+}
+
+void workerRun(FILE * f) {
+  initWorkerContext(f);
+  runTwoJobs();
+}
+
+
+int main(int argc, char **argv) {
   FILE * f = stdin;
   if(f != NULL) {
-    initSequentialContext(f);
-    runOneJob();
+    char command;
+    if((command = getopt(argc, argv, "sw")) != -1) {
+      switch(command) {
+        case 's':
+          sequencialRun(f);
+          break;
+        case 'w':
+          workerRun(f);
+          break;
+      }
+    } else {
+      workerRun(f);
+    }
   } else {
     perror("Error opening input file.");
+    return 1;
   }
+  return 0;
 }
